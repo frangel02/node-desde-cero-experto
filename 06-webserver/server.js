89493fdd9864ad36d6800c3ee4671fe27632d181
frodriguez <frangel_rodriguez@claro.com.dogit config --global user.email frangel_rodriguez@claro.com.do>
2019-07-11 14:27:27 -0400

    Using helpers

--- 06-webserver/server.js
+++ 06-webserver/server.js
@@ -2,25 +2,28 @@ const express = require('express');
 const app = express();
 const hbs = require('hbs');
 
+//Helpers
+require('./hbs/helpers');
+
 app.use(express.static(__dirname + '/public'));
 
 //express HBS engine\
 hbs.registerPartials(__dirname + '/views/parciales');
 app.set('view engine', 'hbs');
 
+
+
+
 app.get('/', (req, res) => {
 
     res.render('home', {
         nombre: 'Frangel',
-        anio: new Date().getFullYear()
     });
 });
 
 app.get('/about', (req, res) => {
 
-    res.render('about', {
-        anio: new Date().getFullYear()
-    });
+    res.render('about', {});
 });
 
 app.listen(3000, () => {
