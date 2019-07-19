05a1cdde67f0618ebfecc8db84da11240f70ade1
frodriguez <frangel_rodriguez@claro.com.dogit config --global user.email frangel_rodriguez@claro.com.do>
2019-07-19 11:08:52 -0400

    ordering routers of users

--- 07-restserver/server/server.js
+++ 07-restserver/server/server.js
@@ -13,41 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario LOCAL!!!');
-});
-
-app.post('/usuario', function(req, res) {
-
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-
-});
-
-app.put('/usuario/:id', function(req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario');
-});
+app.use(require('./routes/usuario'));
 
 mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
     if (err) throw err;
