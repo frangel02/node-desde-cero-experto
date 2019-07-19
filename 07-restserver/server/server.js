01785b7f0150f6bd2a6b0fba14a137707577729c
frodriguez <frangele331@gmail.com>
2019-07-18 23:20:58 -0400

    config  rest server and routes

--- 07-restserver/server/server.js
+++ 07-restserver/server/server.js
@@ -1,36 +1,53 @@
-require('./config/config')
+require('./config/config');
+
 const express = require('express');
 const app = express();
 
 const bodyParser = require('body-parser');
 
-//parse Application/x-www-form-urlencoded
-app.user(bodyParser.urlencoded({extended: false}))
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
-//parse application/json
+// parse application/json
 app.use(bodyParser.json())
 
-app.get('/usuario', function(req, res){
-    res.json('get Usuario');
+
+app.get('/usuario', function(req, res) {
+    res.json('get Usuario LOCAL!!!');
 });
 
-app.post('/usuario', function(req, res){
-    //res.json('get Usuario');
+app.post('/usuario', function(req, res) {
+
     let body = req.body;
-    if(body.nombre === undefined){
+
+    if (body.nombre === undefined) {
+
         res.status(400).json({
             ok: false,
-            mensaje: "El nombre es necesario"
+            mensaje: 'El nombre es necesario'
         });
 
-    }else{
+    } else {
         res.json({
             persona: body
         });
     }
+
 });
 
+app.put('/usuario/:id', function(req, res) {
 
-app.listen(3000, ()=>{
- console.log('Escuchando desde el puerto',process.env.PORT);
+    let id = req.params.id;
+
+    res.json({
+        id
+    });
+});
+
+app.delete('/usuario', function(req, res) {
+    res.json('delete Usuario');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando puerto: ', process.env.PORT);
 });
