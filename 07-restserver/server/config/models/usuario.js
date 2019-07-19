71e4c487d7c8925b0f2e90ea295d35d35f1f3793
frodriguez <frangel_rodriguez@claro.com.dogit config --global user.email frangel_rodriguez@claro.com.do>
2019-07-19 14:03:25 -0400

    Usuario model

---
+++ 07-restserver/server/config/models/usuario.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+let usuarioSchema = new Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es necesario']
+    },
+    email: {
+        type: String,
+        required: [true, 'El email es necesario']
+    },
+    password: {
+        type: String,
+        required: [true, 'la contraseña es necesaria']
+    },
+    img: {
+        type: String,
+        required: false
+    },
+    role: {
+        default: 'USER_ROLE'
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    } //boolean
+
+});
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
