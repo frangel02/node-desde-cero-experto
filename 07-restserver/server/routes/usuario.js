861c079940226da1196f35d2c8cf5b7cd8e8ce6e
frodriguez <frangele331@gmail.com>
2019-08-12 19:23:28 -0400

    filtering and excluding fields

--- 07-restserver/server/routes/usuario.js
+++ 07-restserver/server/routes/usuario.js
@@ -15,7 +15,7 @@ app.get('/usuario', function(req, res) {
     let limite = req.query.limite || 5;
     limite = Number(limite);
 
-    Usuario.find({})
+    Usuario.find({}, 'nombre email role estado google img')
             .skip(desde)
             .limit(limite)
             .exec((err,usuarios)=>{
