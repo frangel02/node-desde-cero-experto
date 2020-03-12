c52abfecb21d0e5cf52c49482af4422c26ccf7e6
frodriguez <frangele331@gmail.com>
2020-03-12 14:27:43 -0400

    Adding login method

--- 07-restserver/server/routes/login.js
+++ 07-restserver/server/routes/login.js
@@ -6,10 +6,49 @@ const app = express();
 
 
 app.post('/login', (req,res)=>{
-    res.json({
-        ok:true
-    })
-})
+
+    let body = req.body;
+
+    Usuario.findOne({email: body.email},(err,UsuarioDB)=>{
+     
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if(!UsuarioDB){
+          
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: "User or password incorrect"
+                }
+            });
+
+        }
+
+        if(!bcrypt.compareSync(body.password, UsuarioDB.password)){
+
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: "User or password incorrect"
+                }
+            });
+
+        }
+
+        res.json({
+            ok: true,
+            usuario: UsuarioDB,
+            token: '123'
+        });
+
+    });
+
+});
 
 
 
