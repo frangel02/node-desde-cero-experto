14fa6396be197995e4a9fd93d3da90d980f04463
frodriguez <frangele331@gmail.com>
2020-03-12 15:10:12 -0400

    Generating token

--- 07-restserver/server/config/config.js
+++ 07-restserver/server/config/config.js
@@ -2,3 +2,27 @@
 //  Puerto
 // ============================
 process.env.PORT = process.env.PORT || 3000;
+
+// ============================
+//  Entorno
+// ============================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ============================
+// Expiration token
+// ============================
+// 60 min
+// 24 h
+// 30 days
+
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+
+// ============================
+// SEED Authentication
+// ============================
+
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo'; 
+
+
--- 07-restserver/server/routes/login.js
+++ 07-restserver/server/routes/login.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const Usuario = require('../models/usuario');
 const app = express();
+var jwt = require('jsonwebtoken');
+
 
 
 
@@ -40,10 +42,13 @@ app.post('/login', (req,res)=>{
 
         }
 
+        let token = jwt.sign({ usuario: UsuarioDB }, process.env.SEED ,{expiresIn: process.env.CADUCIDAD_TOKEN});
+
+
         res.json({
             ok: true,
             usuario: UsuarioDB,
-            token: '123'
+            token
         });
 
     });
