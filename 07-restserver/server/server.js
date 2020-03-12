c2da12a63214259c45199e631e4a67c17f03cc2e
frodriguez <frangele331@gmail.com>
2020-03-12 13:46:41 -0400

    ordering routes of server

--- 07-restserver/server/server.js
+++ 07-restserver/server/server.js
@@ -12,8 +12,8 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-
-app.use(require('./routes/usuario'));
+//Global configuration of routes
+app.use(require('./routes/index'));
 
 mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
     if (err) throw err;
