addeaa3abb74c0665fb9b4ac07b28747a9c22691
frodriguez <frangele331@gmail.com>
2020-03-24 12:48:40 -0400

    google authentication

--- 07-restserver/server/config/config.js
+++ 07-restserver/server/config/config.js
@@ -26,3 +26,8 @@ process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
 process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo'; 
 
 
+// ============================
+//  Google Client ID
+// ============================
+
+process.env.CLIENT_ID = process.env.CLIENT_ID || '219758474264-vh1bibcphgvbc32km508lubtqkanikf1.apps.googleusercontent.com';
